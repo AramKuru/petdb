6870b5f8fb7a902a1073215e9fbea8afeb4424f5
AramKuru <aramjihangir@gmail.com>
2023-08-10 00:14:09 +0300

    db congig

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,12 +1,13 @@
 module.exports = ({ env }) => ({
     connection: {
-      client: 'mysql',
+      client: 'postgres',
       connection: {
         host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 3306),
+        port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
+        schema: env('DATABASE_SCHEMA', 'public'), // Not required
         ssl: {
             ca: env('DATABASE_CA')
         },
