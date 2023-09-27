e8116e249f6ac4b1e09680c8da86a8bf4d60767a
AramKuru <aramjihangir@gmail.com>
2023-09-27 19:27:58 +0300

    Delete Deliveryboy contentType

--- src/api/deliveryboy/routes/deliveryboy.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * deliveryboy router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::deliveryboy.deliveryboy');
--- src/api/deliveryboy/services/deliveryboy.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * deliveryboy service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::deliveryboy.deliveryboy');
