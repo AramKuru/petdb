44accffb7c2aedbd4069e0b837d5deb3b7fe0ef9
AramKuru <aramjihangir@gmail.com>
2023-10-07 15:31:21 +0300

    temprary test lll

--- src/api/order-item/content-types/order-item/lifecycles.js
+++ src/api/order-item/content-types/order-item/lifecycles.js
@@ -1,54 +1,54 @@
 const { errors } = require("@strapi/utils");
 const { ApplicationError } = errors;
 module.exports = {
-  // async beforeCreate(event) {
-  //   const { result, params } = event;
-  //   let { data } = params;
-  //   const product = await strapi.entityService.findOne(
-  //     "api::product.product",
-  //     params.data.product,
-  //     {
-  //       populate: ["discount"],
-  //       fields: ["stock", "price"],
-  //     }
-  //   );
+  async beforeCreate(event) {
+    const { result, params } = event;
+    let { data } = params;
+    const product = await strapi.entityService.findOne(
+      "api::product.product",
+      params.data.product,
+      {
+        populate: ["discount"],
+        fields: ["stock", "price"],
+      }
+    );
 
-  //   // console.log(product, "------  ------", params.data.quantity);
-  //   if (product.stock >= params.data.quantity) {
-  //     await strapi.entityService.update(
-  //       "api::product.product",
-  //       params.data.product,
-  //       {
-  //         data: {
-  //           stock: product.stock - params.data.quantity,
-  //         },
-  //       }
-  //     );
-  //     console.log("date", new Date().toISOString());
-  //     new Date().toISOString() > product.discount.valid_from &&
-  //     new Date().toISOString() < product.discount.valid_until
-  //       ? (data.price =
-  //           Number(product.price) * (1 - product.discount.percent / 100))
-  //       : (data.price = product.price);
+    // console.log(product, "------  ------", params.data.quantity);
+    if (product.stock >= params.data.quantity) {
+      await strapi.entityService.update(
+        "api::product.product",
+        params.data.product,
+        {
+          data: {
+            stock: product.stock - params.data.quantity,
+          },
+        }
+      );
+      console.log("date", new Date().toISOString());
+      new Date().toISOString() > product.discount.valid_from &&
+      new Date().toISOString() < product.discount.valid_until
+        ? (data.price =
+            Number(product.price) * (1 - product.discount.percent / 100))
+        : (data.price = product.price);
 
-  //     // increment order-detail
-  //     // const getorderdetail =
-  //     // await strapi.entityService.findOne("api::order-detail.order-detail",params.data.order_detail,{
-  //     //   fields: [ 'totalPrice','totalProducts']
-  //     // })
-  //     //   console.log(getorderdetail);
-  //     // await strapi.entityService.update("api::order-detail.order-detail",params.data.order_detail, {
-  //     //   data:{
-  //     //     totalPrice: Number(getorderdetail.totalPrice) + (params.data.quantity * data.price),
-  //     //     totalProducts: Number(getorderdetail.totalProducts) + params.data.quantity
-  //     //   }
-  //     // })
-  //   } else
-  //     throw new ApplicationError("Not enough item in stock", {
-  //       stock: product.stock,
-  //     });
-  // },
-  // 
+      // increment order-detail
+      // const getorderdetail =
+      // await strapi.entityService.findOne("api::order-detail.order-detail",params.data.order_detail,{
+      //   fields: [ 'totalPrice','totalProducts']
+      // })
+      //   console.log(getorderdetail);
+      // await strapi.entityService.update("api::order-detail.order-detail",params.data.order_detail, {
+      //   data:{
+      //     totalPrice: Number(getorderdetail.totalPrice) + (params.data.quantity * data.price),
+      //     totalProducts: Number(getorderdetail.totalProducts) + params.data.quantity
+      //   }
+      // })
+    } else
+      throw new ApplicationError("Not enough item in stock", {
+        stock: product.stock,
+      });
+  },
+  
   async afterCreate(event) {
     const { result, params } = event;
     if (result) {
