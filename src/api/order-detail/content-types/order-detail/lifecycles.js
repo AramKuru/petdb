f310b9e619a557ab3b6b58970a0c439ddbe37151
AramKuru <aramjihangir@gmail.com>
2023-10-07 15:06:21 +0300

    temprary test

--- src/api/order-detail/content-types/order-detail/lifecycles.js
+++ src/api/order-detail/content-types/order-detail/lifecycles.js
@@ -1,11 +1,11 @@
 module.exports = {
     // do this on production 
-    // beforeCreate(event) {
-    //   let { data, where, select, populate } = event.params;
+    beforeCreate(event) {
+      let { data, where, select, populate } = event.params;
   
-    //   const ctx = strapi.requestContext.get();
-    //   data.users_permissions_user = ctx.state.user.id;
-    // },
+      const ctx = strapi.requestContext.get();
+      data.users_permissions_user = ctx.state.user.id;
+    },
   
     
   };
