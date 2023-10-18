f39d21b3892d741488dff70650cdb49a3ca18150
AramKuru <aramjihangir@gmail.com>
2023-10-18 22:18:00 +0300

    Remove isOwner to Shippings

--- src/api/shipping/routes/shipping.js
+++ src/api/shipping/routes/shipping.js
@@ -5,14 +5,5 @@
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
-const isOwner = {
-    name: "global::isOwner",
-    config: { contentType: "shipping" },
-};
-module.exports = createCoreRouter('api::shipping.shipping',{
-    config: {
-        find: {
-            policies: [isOwner],
-        }, 
-    },
-});
+
+module.exports = createCoreRouter('api::shipping.shipping');
