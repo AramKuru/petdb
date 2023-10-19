76c199f5fc3f0d698348f2c736d2a2221a3bdf8f
AramKuru <aramjihangir@gmail.com>
2023-10-19 19:40:59 +0300

    Add isOwner to Wishlist

--- src/api/wishlist/routes/wishlist.js
+++ src/api/wishlist/routes/wishlist.js
@@ -5,5 +5,14 @@
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::wishlist.wishlist');
+const isOwner = {
+    name: "global::isOwner",
+    config: { contentType: "wishlist" },
+};
+module.exports = createCoreRouter('api::wishlist.wishlist', {
+    config: {
+        find: {
+            policies: [isOwner],
+        }, 
+    },
+});
