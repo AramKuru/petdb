480c8df5f6b98382c3b18f066fd9781b3decac2e
AramKuru <aramjihangir@gmail.com>
2023-10-19 20:19:14 +0300

    add is owner to orderdetails

--- src/api/order-detail/routes/order-detail.js
+++ src/api/order-detail/routes/order-detail.js
@@ -5,5 +5,14 @@
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::order-detail.order-detail');
+const isOwner = {
+    name: "global::isOwner",
+    config: { contentType: "cart" },
+};
+module.exports = createCoreRouter('api::order-detail.order-detail', {
+    config: {
+        find: {
+            policies: [isOwner],
+        }, 
+    },
+});
