1c76d41e1fb5c140ea509ae5f0974ceb6f652cbe
TOTAL <aramjihangir@gmail.com>
2023-11-28 18:08:02 +0300

    Refactor xl

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -3,7 +3,9 @@ module.exports = ({ env }) => ({
       client: 'postgres',
       connection: {
         host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
+        port: env.int('DATABASE_PORT'
+        // , 5432
+        ),
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
