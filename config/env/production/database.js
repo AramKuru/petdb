4fb512f790766bd56627f280ae7f07d9039df27c
TOTAL <aramjihangir@gmail.com>
2023-11-28 18:18:41 +0300

    Refactor xl

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,19 +1,36 @@
+
 module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: 'postgres',
-      connection: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT'
-        // , 5432
-        ),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        schema: env('DATABASE_SCHEMA', 'public'), // Not required
-        ssl: {
-            ca: env('DATABASE_CA')
-        },
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: {
+        rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
       },
-      debug: false,
     },
-  });
+    debug: false,
+  },
+});
+// module.exports = ({ env }) => ({
+//     connection: {
+//       client: 'postgres',
+//       connection: {
+//         host: env('DATABASE_HOST', '127.0.0.1'),
+//         port: env.int('DATABASE_PORT'
+//         // , 5432
+//         ),
+//         database: env('DATABASE_NAME', 'strapi'),
+//         user: env('DATABASE_USERNAME', 'strapi'),
+//         password: env('DATABASE_PASSWORD', 'strapi'),
+//         schema: env('DATABASE_SCHEMA', 'public'), // Not required
+//         ssl: {
+//             ca: env('DATABASE_CA')
+//         },
+//       },
+//       debug: false,
+//     },
+//   });
