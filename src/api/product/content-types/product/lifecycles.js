6c3f73e30dd65f6a22142a777f694e4f90e71e48
TOTAL <aramjihangir@gmail.com>
2023-12-04 20:28:37 +0300

    Return All categories if clicked on All

--- src/api/product/content-types/product/lifecycles.js
+++ src/api/product/content-types/product/lifecycles.js
@@ -18,6 +18,32 @@ module.exports = {
 
         }
     },
+    // if they choose all so it will bring all the products
+    // async afterFindMany(event) {
+    //   const { params } = event;  
+    //     const entry = await strapi.entityService.findOne('api::category.category', params?.where?.categories?.id?.$eq, {
+    //        fields: ['id','name'],
+    //       populate: {
+    //         animal: {
+    //           fields: ['id' ],
+    //           populate: {
+    //             categories: {fields: ['id']},
+    //           },
+    //         },
+    //       },
+    //     });
+    //     // params.where.categories.id.$eq = 2; 
+    //     console.log(entry?.animal?.categories);
+    //     if(entry?.name?.toLowerCase() == "all") { 
+    //     console.log(entry?.animal?.id);
+    //     console.log(params?.where?.categories?.id?.$eq);
+    //     // entry?.animal?.categories.map((x,v)=>{console.log(x,v);})
+    //     delete params?.where?.categories?.id?.$eq
+    //     params.where.categories.id.$eq = 2; 
+    //     // params.where.categories.id.$eq[0] = 2;
+    //     // { '$or': [ { categories: {id:1} }, { categories: {id:2} } ] }
+    //     }
+    // },
     // async beforeFindMany(event) {
     //   const { result, params } = event;
     //   strapi.notification.sendNotification("e4xeH6V9QQKZIu0FJMnC55:APA91bGYQ0XOUCVruW1PoYxertjC2DXP2sKyv1p1dVBV-MW5zjErvL8nz1E1bR4hOc7yg2J5Lc5cHAVU9sTS8QLd1yVDYTsFnn6Eo4eniEhA18UdrXymFG2M9j8ubB9BpFg-1RcchJ1J", {
--- src/api/product/controllers/product.js
+++ src/api/product/controllers/product.js
@@ -6,4 +6,37 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::product.product');
+module.exports = createCoreController('api::product.product',({ strapi }) =>  ({
+
+  
+    // Method 2: Wrapping a core action (leaves core logic in place)
+    async find(ctx) {
+      // some custom logic here
+      ctx.query = { ...ctx.query, local: 'en' }
+  console.log(ctx?.query?.filters?.categories?.id?.$eq);
+  
+  const entry = await strapi.entityService.findOne('api::category.category', ctx?.query?.filters?.categories?.id?.$eq, {
+    fields: ['id','name'],
+   populate: {
+     animal: {
+       fields: ['id' ],
+       populate: {
+         categories: {fields: ['id']},
+       },
+     },
+   },
+ });
+ ctx.query.filters.categories.id.$eq = entry?.animal?.categories.map((obj) => obj.id)
+
+ console.log(entry?.animal?.categories.map((obj) => obj.id));
+      // Calling the default core action
+      const { data, meta } = await super.find(ctx);
+  
+      // some more custom logic
+    //   meta.date = Date.now()
+  
+      return { data, meta };
+    },
+  
+
+  }));
