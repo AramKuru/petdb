1c783365c26e1b6adc67dbbbbdbcee621a0a86bb
TOTAL <aramjihangir@gmail.com>
2023-12-19 00:12:28 +0300

    Refactor

---
+++ server.js
@@ -0,0 +1,3 @@
+// //10-12-23 for linode based on this article https://medium.com/@leodeo/deploying-strapi-version-4-on-linode-or-any-cloud-host-using-pm2-via-github-and-accessing-it-from-da87b52b7202
+// const strapi = require('@strapi/strapi');
+// strapi().start();
