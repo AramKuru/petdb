fd18805a41cdf67ce716e6071116b698752b3fbd
TOTAL <aramjihangir@gmail.com>
2024-01-15 20:26:35 +0300

    Refactor

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -7,7 +7,10 @@ module.exports = ({ env }) => ({
       port: env.int('DATABASE_PORT'),
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD')
+      password: env('DATABASE_PASSWORD'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
     },
     debug: false,
   },
