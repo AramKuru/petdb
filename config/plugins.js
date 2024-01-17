196a8b0ba617690d9b1c625268d69052635edd70
TOTAL <aramjihangir@gmail.com>
2024-01-17 11:05:56 +0300

    Gmail Sender

--- config/plugins.js
+++ config/plugins.js
@@ -21,4 +21,22 @@
             },
           },
         },
+        email: {
+          config: {
+            provider: 'gmail-api',
+            providerOptions: {
+              auth: {
+                userId: env('EMAIL_OAUTH2_USERID'),
+                clientId: env('EMAIL_OAUTH2_CLIENTID'),
+                clientSecret: env('EMAIL_OAUTH2_CLIENTSECRET'),
+                refreshToken: env('EMAIL_OAUTH2_REFRESHTOKEN'),
+              },
+            },
+            settings: {
+              defaultFrom: env('EMAIL_FROM'),
+              defaultReplyTo: env('EMAIL_REPLYTO'),
+              testAddress: env('EMAIL_TEST_ADDRESS'),
+            },
+          },
+        },
   });
